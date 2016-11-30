87f18154eea8a894dd68b4a7f358e65d7b9a6dba
anorthrup0325 <anthonyn@uoregon.edu>
2016-11-30 13:48:58 -0800

    Added alternate video links

--- Other/Arrowverse/main.js
+++ Other/Arrowverse/main.js
@@ -17,50 +17,85 @@ var dom_btn_prev = null,
 	dom_btn_next = null,
 	dom_cur_link = null,
 	dom_cur = null,
+	dom_btn_source_prev = null,
+	dom_btn_source_next = null,
+	dom_source_link = null,
+	dom_source = null,
 	dom_frame = null,
-	dom_ep_list;
+	dom_ep_list = null;
 function load_dom() {
 	dom_btn_prev = document.getElementById("btn-prev");
 	dom_btn_next = document.getElementById("btn-next");
 	dom_cur_link = document.getElementById("cur-link");
 	dom_cur = document.getElementById("cur");
+	dom_btn_source_prev = document.getElementById("btn-source-prev");
+	dom_btn_source_next = document.getElementById("btn-source-next");
+	dom_source_link = document.getElementById("source-link");
+	dom_source = document.getElementById("source");
 	dom_frame = document.getElementById("frame");
 	dom_ep_list = document.getElementById("ep-list");
 }
 
 // State
 var index = 0,
-	ls_item = "arrowverse_index";
+	source = "adhqmedia.com",
+	source_index = 0,
+	sources = [],
+	source_lookup = {},
+	ls_index = "arrowverse_index",
+	ls_source = "arrowverse_source";
 
 // Local storage
 var has_LS = window.localStorage !== undefined;
 function load() {
 	if(has_LS) {
-		index = parseInt(window.localStorage.getItem(ls_item));
+		index = parseInt(window.localStorage.getItem(ls_index));
 		if(isNaN(index)) {
 			index = 0;
+		}
+		source = window.localStorage.getItem(ls_source);
+		if(source === null) {
+			source = "adhqmedia.com";
 		}
 	}
 	refresh_display();
 }
 function save() {
 	if(has_LS) {
-		window.localStorage.setItem(ls_item, index.toString());
+		window.localStorage.setItem(ls_index, index.toString());
+		window.localStorage.setItem(ls_source, source);
 	}
 }
 
 // Video links
-var best_links = ["adhqmedia.com", "lolzor.com", "openload.co", "vidbaba.com"];
-function get_video_link(info) {
-	var res = "missing.html", site, site_link;
-	for(var i = 0; i < best_links.length && res == "missing.html"; i++) {
-		site = best_links[i];
-		site_link = info["vid_links"][site];
-		if(site_link !== undefined) {
-			res = site_link;
+var best_sources = ["adhqmedia.com", "lolzor.com", "openload.co", "vidbaba.com"];
+function load_sources(info) {
+	source_lookup = info["vid_links"];
+	sources = Object.keys(source_lookup).sort();
+	if(source_lookup[source] === undefined) {
+		source_index = -1;
+		var best_source;
+		for(var i = 0; i < best_sources.length; i++) {
+			best_source = best_sources[i];
+			if(source_lookup[best_source] !== undefined) {
+				source_index = sources.indexOf(best_source);
+				return "Auto";
+			}
 		}
+		source_index = 0;
+		return "Default";
 	}
-	return res;
+	if(sources[source_index] != source) {
+		source_index = sources.indexOf(source);
+	}
+	return false;
+}
+
+// Set source
+function refresh_source() {
+	// Update source name
+	source = sources[source_index];
+	save();
 }
 
 // Set display
@@ -78,6 +113,9 @@ function refresh_display() {
 		content += "<br /><i>&quot;" + info["title"] + "&quot;</i>";
 	}
 	dom_cur.innerHTML = content;
+    
+    // Set current link
+    dom_cur_link.setAttribute("href", info["ep_link"]);
 	
 	// Set previous info
 	if(index <= 0) {
@@ -95,8 +133,39 @@ function refresh_display() {
 		dom_btn_next.innerHTML = "Next: " + next_info["show"] + " " + next_info["season"] + "-" + next_info["episode"];
 	}
 	
+	// Get video
+	var used_source = load_sources(info);
+	var source_name = sources[source_index];
+
+	// Set source info
+	var source_content = "";
+	if(used_source) {
+		source_content = " (" + used_source + ")";
+	}
+	dom_source.innerHTML = "Source:" + source_content + "<br /><i>" + source_name + "</i>";
+
+	// Set previous source info
+	if(source_index <= 0) {
+		dom_btn_source_prev.innerHTML = "Previous: N / A";
+	} else {
+		var prev_source = sources[source_index - 1];
+		dom_btn_source_prev.innerHTML = "Previous: " + prev_source;
+	}
+
+	// Set next source info
+	if(source_index >= sources.length - 1) {
+		dom_btn_source_next.innerHTML = "Next: N / A";
+	} else {
+		var next_source = sources[source_index + 1];
+		dom_btn_source_next.innerHTML = "Next: " + next_source;
+	}
+
+	// Set source link
+	var source_link = source_lookup[source_name];
+	dom_source_link.href = source_link;
+	
 	// Display video
-	dom_frame.setAttribute("src", get_video_link(info));
+    dom_frame.setAttribute("src", source_link);
 }
 
 // Callback to display episode
@@ -146,5 +215,21 @@ function goto_next() {
 		return;
 	}
 	index++;
+	refresh_display();
+}
+function source_prev() {
+	if(source_index <= 0) {
+		return;
+	}
+	source_index--;
+	refresh_source();
+	refresh_display();
+}
+function source_next() {
+	if(source_index >= sources.length - 1) {
+		return;
+	}
+	source_index++;
+	refresh_source();
 	refresh_display();
 }
